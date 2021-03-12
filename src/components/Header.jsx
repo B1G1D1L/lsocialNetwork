395a1467f51968d84c85ee962afcc1584397110e
unknown <Senognoevilya@gmail.com>
2021-03-12 17:18:49 +0500

    add new project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://s.cdpn.io/3/kiwi.svg'></img>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar  = () => {
+  return (
+    <nav className='nav'>
+      <a href='#4'>Profile</a>
+      <a href='#4'>Message</a>
+      <a href='#4'>News</a>
+      <a href='#4'>Music</a>
+      <a href='#4'>Setting</a>
+    </nav>
+  )
+}
+
+export default Navbar;
