007757e0c651af18f3a3b5b19d1f6b42bf516dc2
ваше имя <Senognoevilya@gmail.com>
2021-05-18 18:24:12 +0500

    added a get request to the server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,29 @@
 import React from 'react';
 import style from './Users.module.css';
+// import * as axios from 'axios';
+import axios from 'axios'
+import userPhoto from '../../assets/images/user.png';
+
 
 const Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photo: 'https://i05.fotocdn.net/s113/b8d246c1b6fd8335/user_xl/2538414040.jpg',
-        followed: true,
-        fullName: 'Yura',
-        status: 'I am imba',
-        location: { sity: 'Prokutkino', country: 'Russia' }
-      },
-      {
-        id: 2,
-        photo: 'https://i05.fotocdn.net/s113/b8d246c1b6fd8335/user_xl/2538414040.jpg',
-        followed: true,
-        fullName: 'Vasya',
-        status: 'I am Kat',
-        location: { sity: 'Moscow', country: 'Russia' }
-      },
-      {
-        id: 3,
-        photo: 'https://i05.fotocdn.net/s113/b8d246c1b6fd8335/user_xl/2538414040.jpg',
-        followed: false,
-        fullName: 'Yura',
-        status: 'I am imba',
-        location: { sity: 'Tymen', country: 'Russia' }
-      },
-    ])
+    
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items);
+      })
+
   }
 
   return <div>
     {
       props.users.map(user => <div key = {user.id} className ={style.user}>
         <div className = {style.userAvatar}>
-          <img className = {style.photo} src={user.photo} alt="Avatar" />
+
+          <img className = {style.photo}
+              src={user.photos.large !== null ? user.photos.large: userPhoto}
+              alt="Avatar" />
 
           { user.followed 
             ? <button onClick = { () => props.unfollow(user.id) }>Unfollow</button> 
@@ -46,14 +33,17 @@ const Users = (props) => {
   
         <div className = {style.userInfo}>
           <div className = {style.name}>
-            <strong>{user.fullName}</strong>
+
+            <strong>{user.name}</strong>
+
             <div className = {style.status}>{user.status}</div>
+            
           </div>
   
           <div>
             <div className = {style.location}>
-              <span >{user.location.country}</span>
-              <span >{user.location.sity}</span>
+              {/* <span >{user.location.country}</span>
+              <span >{user.location.sity}</span> */}
             </div>
           </div>
         </div>
