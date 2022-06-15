05112a72d802c540134d8b5960d5b11cd48f5d02
unknown <senognoevilya@gmail.com>
2022-06-15 11:51:37 +0500

    add: folder

--- src/pages2/index.tsx
+++ src/pages2/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 const FeedPage = React.lazy(() => import('./feed'))
 const MessagePage = React.lazy(() => import('./message'))
 
 export const Routing = () => (
   <Routes>
+    <Route path="/" element={<Navigate replace to="feed" />} />
     <Route path="feed" element={<FeedPage />} />
     <Route path="mes sages" element={<MessagePage />} />
   </Routes>
--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import cn from 'classnames'
 
-import styles from './Button.module.css'
+import styles from './button.module.css'
 
 // Types
 interface PropsType {
