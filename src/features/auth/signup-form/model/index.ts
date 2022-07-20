fc840eae51737e8449c53339e6631be152649b7e
ваше имя <Senognoevilya@gmail.com>
2022-07-20 18:52:04 +0500

    update

--- src/features/auth/signup-form/model/index.ts
+++ src/features/auth/signup-form/model/index.ts
@@ -1,7 +1,5 @@
-import {userModal} from 'entities/user'
+import { userModal } from 'entities/user'
 import { SignUp, typicodeApi } from 'shared/api'
-
-// export const createAccountFx = createEffect(typicodeApi.auth.createAccount)
 
 export const createAccountFx = async (data: SignUp) => {
   await typicodeApi.auth.createAccountApi(data, userModal.updateUser)
