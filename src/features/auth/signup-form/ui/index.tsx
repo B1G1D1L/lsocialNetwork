0b3cb6158517b2a352d6c8aad70352e566b60f4c
Ilya <62830106+Ilya006@users.noreply.github.com>
2022-07-28 22:34:08 +0500

    refactor:  add redirect after auth

--- src/features/auth/signup-form/ui/index.tsx
+++ src/features/auth/signup-form/ui/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Form, Formik } from 'formik'
+import { FirebaseError } from 'firebase/app'
 import { createEffect, sample } from 'effector/compat'
+import { useStore } from 'effector-react/compat'
+import { Link, Navigate } from 'react-router-dom'
 
 import { signupModel } from 'features/auth/signup-form'
+import { $isAuth } from 'entities/user/model'
 import { Button, Field } from 'shared/ui'
 import type { ISignUp } from 'shared/api'
 import { errorsCode, SignupSchema } from '../lib'
 
 import styles from './styles.module.css'
-import { Link } from 'react-router-dom'
-import { FirebaseError } from 'firebase/app'
 
 const initialValues: ISignUp = {
   name: '',
@@ -18,6 +20,8 @@ const initialValues: ISignUp = {
 }
 
 export const SignupForm = () => {
+  const isAuth = useStore($isAuth)
+
   const handleSubmit = (data: ISignUp, actions: any) => {
     signupModel.createAccountFx(data)
 
@@ -32,6 +36,10 @@ export const SignupForm = () => {
     })
   }
 
+  if (isAuth) {
+    return <Navigate to="/feed" />
+  }
+
   return (
     <Formik
       initialValues={initialValues}
@@ -43,7 +51,9 @@ export const SignupForm = () => {
         <Field name="email" placeholder="Email" />
         <Field name="password" placeholder="Password" type="password" />
         <Button type="submit">Sign Up</Button>
-        <Link to="/signin">Authorization</Link>
+        <Link to="/signin" className={styles.link}>
+          Authorization
+        </Link>
       </Form>
     </Formik>
   )
